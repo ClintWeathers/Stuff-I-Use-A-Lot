b9516fc65ac876385fd559a21cd29eaf9e765b7e
Clint <Clint@KwanUmBosal.local>
2013-04-07 07:08:30 -0500

    added the infochimps geocoder and a simple .bashrc file I like

---
+++ infochimps_geocoder.pl
@@ -0,0 +1,68 @@
+# This is a basic perl script to geocode addresses using InfoChimps' geocoding API.
+# There are better ways to geocode and easier ways than this, but it was a good
+# exercise in using Perl to hit an API. 
+# Your infile needs to have one address on it per line. 
+
+
+#!/opt/local/bin/perl5.12
+
+use strict;
+use WWW::Mechanize;
+use warnings;
+
+my $file    = $ARGV[0];
+my $outfile = "outfile.txt";
+open( FILE, "<$file" ) || die " Cannot open file $file : $!\n";
+open( MYFILE, ">$outfile" );
+
+my $url     = '';
+my @data    = <FILE>;
+my $addy    = '';
+my $bareurl = '';
+my $mech    = WWW::Mechanize->new( autocheck => 1 );
+my $recnum  = 0;
+
+$bareurl =
+'http://api.infochimps.com/geo/utils/geolocate?apikey=YourAPIKeyGoesHere&f.address_text=';
+
+foreach $addy (@data) {
+    $url = $bareurl . $addy;
+    $mech->get($url);
+
+    print "\n";
+    print MYFILE "\n";
+
+    if ( $mech->content =~
+        /([-]?[0-9]{1,3}\.[0-9]{1,30}\,[-]?[0-9]{1,3}\.[0-9]{1,30})/ )
+    {
+        my $latlon = $1;
+        print $latlon. " ";
+        print MYFILE $latlon . " ";
+    }
+
+    if ( $mech->content =~ /(\:0\.[0-9]{1,3})/ ) {
+        my $confidence = $1;
+        my ( $con1, $con2 ) = split( ":", $confidence );
+        print $con2;
+        print MYFILE $con2;
+        if ( $con2 < 0.51 ) {
+            print "\tLow confidence";
+            print MYFILE "\tLow Confidence";
+        }
+        else {
+            print "\tHigh confidence";
+            print MYFILE "\tHigh Confidence";
+        }
+
+        #print "\n";
+        #print MYFILE "\n";
+        $recnum++;
+        sleep(3);
+    }
+
+}
+print "\n";
+print $recnum . " records in total.\n";
+print MYFILE $recnum . " records in total\n";
+close FILE;
+close MYFILE;
---
+++ simple_bashrc.sh
@@ -0,0 +1,41 @@
+#  rename this to .bashrc
+#  put it in your ~ directory
+ 
+# Colors
+ 
+ 
+Black="$(tput setaf 0)"
+BlackBG="$(tput setab 0)"
+DarkGrey="$(tput bold ; tput setaf 0)"
+LightGrey="$(tput setaf 7)"
+LightGreyBG="$(tput setab 7)"
+White="$(tput bold ; tput setaf 7)"
+Red="$(tput setaf 1)"
+RedBG="$(tput setab 1)"
+LightRed="$(tput bold ; tput setaf 1)"
+Green="$(tput setaf 2)"
+GreenBG="$(tput setab 2)"
+LightGreen="$(tput bold ; tput setaf 2)"
+Brown="$(tput setaf 3)"
+BrownBG="$(tput setab 3)"
+Yellow="$(tput bold ; tput setaf 3)"
+Blue="$(tput setaf 4)"
+BlueBG="$(tput setab 4)"
+LightBlue="$(tput bold ; tput setaf 4)"
+Purple="$(tput setaf 5)"
+PurpleBG="$(tput setab 5)"
+Pink="$(tput bold ; tput setaf 5)"
+Cyan="$(tput setaf 6)"
+CyanBG="$(tput setab 6)"
+LightCyan="$(tput bold ; tput setaf 6)"
+NC="$(tput sgr0)" # No Color
+ 
+PS1='[\[$Red\]\@ \[$White\]:: \[$LightRed\]\u@\h\[$White\]] \w \$ \[$NC\]'
+ 
+# Source global definitions
+if [ -f /etc/bashrc ]; then
+	. /etc/bashrc
+fi
+ 
+# User specific aliases and functions
+alias ls='ls -hF --color'
