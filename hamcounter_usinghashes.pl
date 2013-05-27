de4c3adc58586c1023300b648c02e66fc08778ac
Clint Weathers <clintweathers@gmail.com>
2013-05-26 19:55:47 -0500

    YAHC (Yet Another Hamlet Counter)
    For each language I learn/relearn, I like to get a grip on the internet
    basics, pattern matching and control features (loops, etc) using a
    pretty simple exercize:
    
    Go out to MIT's Shakespeare site, grab the full text version of Hamlet,
    and count the number of times the word "Hamlet" comes up in any
    context.  It should be 85.
    
    I'm going back through Learning Perl, and when I hit the chapter on
    hashes, I decided to finally wrap my head around them once and forall
    using the HamCounter.
    
    I also have HamCounters in Python and R, and will upload those at a
    later time.

---
+++ hamcounter_usinghashes.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/perl
+use strict;
+use LWP::Simple;
+
+&countWords();
+my %wordCount;
+
+print "print the hash of words: \n";
+foreach my $key (keys %wordCount) {
+    if ($key =~ /Hamlet/) {
+        print "$key => $wordCount{$key}\n";
+    }
+}
+
+sub countWords()
+{
+    my $line = get('http://shakespeare.mit.edu/hamlet/full.html')
+      or die 'Unable to get the page. Sorry.';
+
+    my @line_words = split(/\W/, $line);
+    foreach my $word (@line_words) {
+        if ($wordCount{$word}) {
+            $wordCount{$word}++;
+        }
+        else {
+            $wordCount{$word} = 1;
+        }
+    }
+}
